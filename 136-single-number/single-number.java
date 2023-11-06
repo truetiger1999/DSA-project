69053eb0f58687661ddd04d5336c008eaf6a32f1
truetiger1999 <rickykoss63@gmail.com>
2023-11-06 14:17:12 +0530

    Time: 1 ms (99.92%) | Memory: 44.3 MB (69.60%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,23 +1,10 @@
 class Solution {
   public int singleNumber(int[] nums) {
-    int count = 0;
-    int num = 0;
-
-    for (int i = 0; i < nums.length; i++) {
-      num = nums[i];
-      count = 0;
-      for (int j = 0; j < nums.length; j++) {
-        if (nums[j] == num) {
-          count++;
-        }
-      }
-
-      if (count == 1) {
-        return num;
-      }
+    
+    int xor = 0;
+    for(int i =0; i<nums.length; i++){
+        xor = xor^nums[i];
     }
-
-    // If there is no single number in the array, return -1.
-    return -1;
+    return xor;
   }
 }
