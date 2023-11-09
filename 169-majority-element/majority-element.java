15ba2a657e7bf1287cc94a280a0d171000962c31
truetiger1999 <rickykoss63@gmail.com>
2023-11-09 09:47:08 +0530

    Time: 11 ms (34.49%) | Memory: 46.8 MB (77.64%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,8 +1,27 @@
 class Solution {
 
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+    public int majorityElement(int[] v) {
+        //This is the burte force approach
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // return nums[n/2];
+
+        //This is the better approach
+        int n = v.length;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(v[i], 0);
+            mpp.put(v[i], value + 1);
+        }
+
+        //searching for the majority element:
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                return it.getKey();
+            }
+        }
+
+        return -1;
     }
 }
