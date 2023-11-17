e025a248e1a0844163c561948578f1a7f415d7c2
truetiger1999 <rickykoss63@gmail.com>
2023-11-18 02:51:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (28.23%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,31 +1,15 @@
 class Solution {
+
     public int missingNumber(int[] arr) {
-        int i = 0;
-        while(i<arr.length){
-            int correct = arr[i];
-            if( arr[i]<arr.length && arr[i] != arr[correct]){
-                swap(arr , i , correct);
-            }else{
-                i++;
-            }
+        int N = arr.length;
+        int sum = (N * (N + 1)) / 2;
+        int s2 = 0;
+
+        for (int i = 0; i < N; i++) {
+            s2 += arr[i];
         }
 
-        //search for first missing number
-
-        for (int index = 0; index<arr.length; index++){
-            if(arr[index]!= index){
-                return index;
-            }
-
-        }
-        //case 2
-        return arr.length;
-
-    }
-    static void swap(int[] arr, int first , int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second]  = temp;
-
+        int missingNum = sum - s2;
+        return missingNum;
     }
 }
