6aaac37196dea0160b928097f9f3343b298a3a71
truetiger1999 <rickykoss63@gmail.com>
2023-11-24 19:38:03 +0530

    Time: 16 ms (89.76%) | Memory: 57.7 MB (6.12%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,49 +1,44 @@
 class Solution {
-      public int minDays(int[] bloomDay, int m, int k) {
-        long val=(long)m*k;
-        if( val > bloomDay.length) return -1;
-
-        int low=Integer.MAX_VALUE;
-        int high=Integer.MIN_VALUE;
-
-        for(int i=0;i<bloomDay.length;i++){
-            low=Math.min(low,bloomDay[i]);
-            high=Math.max(high,bloomDay[i]);
+      public int minDays(int[] arr, int m, int k) {
+         long val = (long) m * k;
+        int n = arr.length; 
+        if (val > n) return -1; 
+       
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
         }
 
-        while(low<=high){
-            int mid=(low+high)/2;
-
-            if(possibleDays(bloomDay,m,k,mid)){
-                high=mid-1;
+        
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(arr, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-            else{
-                low=mid+1;
-            }
-           
         }
         return low;
 
 
     }
 
-    public boolean possibleDays(int[] bloomDay, int m, int k,int mid){
-        
-        int totalBouquet=0;
-        int window=0;
-        for(int i=0;i<bloomDay.length;i++){
-            if(bloomDay[i]<=mid){
-                window++;
-            }
-            else{
-                totalBouquet+=(window/k);
-                window=0;
-
+    public static boolean possible(int[] arr, int day, int m, int k) {
+        int n = arr.length; 
+        int cnt = 0;
+        int noOfB = 0;
+       
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
+                cnt++;
+            } else {
+                noOfB += (cnt / k);
+                cnt = 0;
             }
         }
-
-        totalBouquet+=(window/k);
-        return totalBouquet>=m;
-
+        noOfB += (cnt / k);
+        return noOfB >= m;
     }
 }
