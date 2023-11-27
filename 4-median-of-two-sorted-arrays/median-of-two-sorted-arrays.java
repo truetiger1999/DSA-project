8d27f32d37f011bb376da12fb5096ac33f7282a9
truetiger1999 <rickykoss63@gmail.com>
2023-11-27 21:38:38 +0530

    Time: 2 ms (67.08%) | Memory: 44.7 MB (35.62%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,37 @@
+class Solution {
+    public double findMedianSortedArrays(int[] a, int[] b) {
+          // Size of two given arrays
+            // Size of two given arrays
+        int n1 = a.length;
+        int n2 = b.length;
+
+        List<Integer> arr3 = new ArrayList<>();
+        // Apply the merge step
+        int i = 0, j = 0, k = 0;
+        while (i < n1 && j < n2) {
+            if (a[i] < b[j]) {
+                arr3.add(a[i++]);
+            } else {
+                arr3.add(b[j++]);
+            }
+        }
+
+        // Copy the left-out elements
+        while (i < n1) {
+            arr3.add(a[i++]);
+        }
+        while (j < n2) {
+            arr3.add(b[j++]);
+        }
+
+        // Find the median
+        int n = n1 + n2;
+        if (n % 2 == 1) {
+            return (double) arr3.get(n / 2);
+        }
+
+        double median = ((double) arr3.get(n / 2) + (double) arr3.get((n / 2) - 1)) / 2.0;
+        return median;
+        
+    }
+}
