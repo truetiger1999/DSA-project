7719eba6a3cc25df75564a6f1ef1d2c379711e3f
truetiger1999 <rickykoss63@gmail.com>
2024-01-05 20:34:34 +0530

    Time: 1 ms (61.62%) | Memory: 41.4 MB (15.26%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int left =0, right = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                left++;
+            }
+            else{
+                if(left>0){
+                    left--;
+                }
+                else{
+                    right++;
+                }
+            }
+        }
+
+        return left+right;
+    }
+}
