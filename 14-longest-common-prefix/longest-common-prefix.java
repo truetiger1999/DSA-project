eee06f93eedf47c2929d3958ddaaf6b90ef7c7a6
truetiger1999 <rickykoss63@gmail.com>
2024-02-09 10:34:34 +0530

    Time: 1 ms (80.50%) | Memory: 42 MB (14.69%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,16 +3,16 @@ class Solution {
         Arrays.sort(strs);
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
+
         int idx = 0;
-        while(idx<s1.length() && idx<s2.length()){
-            if(s1.charAt(idx)==s2.charAt(idx))
-            {
+        while(idx<s1.length()){
+            if(s1.charAt(idx)==s2.charAt(idx)){
                 idx++;
             }
             else{
                 break;
             }
         }
-        return s1.substring(0,idx);
+        return s1.substring(0, idx);
     }
 }
