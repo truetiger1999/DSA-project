c7f57c10e7ab93095639d3ecd465bcb759b78706
truetiger1999 <rickykoss63@gmail.com>
2024-02-12 11:08:46 +0530

    Time: 4 ms (55.98%) | Memory: 50.9 MB (15.30%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,54 +1,6 @@
 class Solution {
-
-    public int majorityElement(int[] v) {
-        //This is the burte force approach
-        // Arrays.sort(nums);
-        // int n = nums.length;
-        // return nums[n/2];
-
-        //This is the better approach
-        // int n = v.length;
-        // HashMap<Integer, Integer> mpp = new HashMap<>();
-
-        // for (int i = 0; i < n; i++) {
-        //     int value = mpp.getOrDefault(v[i], 0);
-        //     mpp.put(v[i], value + 1);
-        // }
-
-        // //searching for the majority element:
-        // for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
-        //     if (it.getValue() > (n / 2)) {
-        //         return it.getKey();
-        //     }
-        // }
-
-        // return -1;
-
-        //Now we will be looking for the Optimal Approach
-
-        int cnt = 0;
-        int el = v[0];
-        for(int i =0 ; i<v.length; i++){
-            if(cnt ==0){
-                cnt =1;
-                el = v[i];
-            }
-            else if(v[i] ==el){
-                cnt++;
-            }
-            else{
-                cnt--;
-            }
-        }
-        int cnt1 = 0;
-        for(int i=0; i<v.length; i++){
-            if(v[i] == el)cnt1++;
-        }
-        if(cnt1>(v.length/2))
-        {
-            return el;
-        }
-
-        return -1;
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
