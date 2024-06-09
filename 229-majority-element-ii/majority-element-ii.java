dc509cde74abeba0d04f4adbc41ca95603755028
truetiger1999 <rickykoss63@gmail.com>
2024-06-09 10:44:35 +0530

    Time: 16 ms (10.72%) | Memory: 47.8 MB (58.39%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,37 +1,23 @@
 class Solution {
-
     public List<Integer> majorityElement(int[] v) {
+        List<Integer> ls  = new ArrayList<>();
         int n = v.length;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
 
-        int cnt1 = 0, cnt2 = 0;
-        int el1 = Integer.MIN_VALUE;
-        int el2 = Integer.MIN_VALUE;
+        int min = (int)(n/3) + 1;
 
-        for (int i = 0; i < n; i++) {
-            if (cnt1 == 0 && el2 != v[i]) {
-                cnt1 = 1;
-                el1 = v[i];
-            } else if (cnt2 == 0 && el1 != v[i]) {
-                cnt2 = 1;
-                el2 = v[i];
-            } else if (v[i] == el1) cnt1++; else if (v[i] == el2) cnt2++; else {
-                cnt1--;
-                cnt2--;
+        for(int i=0; i<n; i++){
+            int value = mpp.getOrDefault(v[i],0);
+            mpp.put(v[i] , value+1);
+
+
+            if(mpp.get(v[i])==min){
+                ls.add(v[i]);
+            }
+            if(ls.size()==2){
+                break;
             }
         }
-
-        List<Integer> ls = new ArrayList<>();
-
-        cnt1 = 0;
-        cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == el1) cnt1++;
-            if (v[i] == el2) cnt2++;
-        }
-
-        int mini = (int) (n / 3) + 1;
-        if (cnt1 >= mini) ls.add(el1);
-        if (cnt2 >= mini) ls.add(el2);
 
         return ls;
     }
