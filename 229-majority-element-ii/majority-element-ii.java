da483f698f8ab3015ebc6bc9a3860e510d62fd4f
truetiger1999 <rickykoss63@gmail.com>
2024-06-09 10:59:33 +0530

    Time: 3 ms (70.70%) | Memory: 48.3 MB (14.58%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] v) {
         List<Integer> ls  = new ArrayList<>();
+        
+        //sbse phle majority element more than n/2 vala logic apply karengee
         int n = v.length;
-        HashMap<Integer, Integer> mpp = new HashMap<>();
-
-        int min = (int)(n/3) + 1;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+        int cnt1 = 0, cnt2 = 0;
 
         for(int i=0; i<n; i++){
-            int value = mpp.getOrDefault(v[i],0);
-            mpp.put(v[i] , value+1);
-
-
-            if(mpp.get(v[i])==min){
-                ls.add(v[i]);
+            if(cnt1==0 && ele2!=v[i]){
+                cnt1 = 1;
+                ele1 = v[i];
             }
-            if(ls.size()==2){
-                break;
+            else if(cnt2==0 && ele1!=v[i]){
+                cnt2 = 1;
+                ele2 = v[i];
+            }
+            else if(v[i]==ele1) cnt1++;
+            else if(v[i]==ele2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+
+        //now we are manually checking the ele1 and ele2
+
+        cnt1 = 0 ; cnt2 = 0 ;
+        for(int i=0; i<n; i++){
+            if(v[i]==ele1) cnt1++;
+            if(v[i]==ele2) cnt2++;
+        }
+
+        int mini = (int)(n/3) +1;
+        if(cnt1>=mini) ls.add(ele1);
+        if(cnt2>=mini) ls.add(ele2);
 
         return ls;
     }
