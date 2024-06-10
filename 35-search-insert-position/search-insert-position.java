219f0661bd48770eb149dfa5dc0f3933b16a0632
truetiger1999 <rickykoss63@gmail.com>
2024-06-11 01:34:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (92.78%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,21 +1,22 @@
 class Solution {
     public int searchInsert(int[] arr, int target) {
+
+        //this is exactly similar to lower bound
+        int n = arr.length;
         int low = 0;
-        int high = arr.length-1;
-        int ans = arr.length;
+        int high = n-1;
+        int ans  = n;
         while(low<=high){
             int mid = (low+(high-low)/2);
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]<target){
-                low = mid+1;
-            }
-            else{
+            if(arr[mid]>=target){
                 ans = mid;
                 high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
+
         return ans;
+        
     }
 }
