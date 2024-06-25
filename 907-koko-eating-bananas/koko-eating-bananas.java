d2d4e1fe85280dc1d85d471e245054d22f362c9c
truetiger1999 <rickykoss63@gmail.com>
2024-06-25 11:24:56 +0530

    Time: 26 ms (37.62%) | Memory: 44.9 MB (50.28%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,36 +1,41 @@
 class Solution {
-    public static int findMax(int[] v) {
-        int maxi = Integer.MIN_VALUE;;
-        int n = v.length;
-        //find the maximum:
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, v[i]);
-        }
-        return maxi;
-    }
 
-    public static int calculateTotalHours(int[] v, int hourly) {
-        int totalH = 0;
-        int n = v.length;
-        //find total hours:
-        for (int i = 0; i < n; i++) {
-            totalH += Math.ceil((double)(v[i]) / (double)(hourly));
+    static int findMax(int[] piles){
+            int maxi = Integer.MIN_VALUE;
+            int n = piles.length;
+            for(int i=0; i<n;i++){
+                maxi = Math.max(piles[i], maxi);
+            }
+            return maxi;
         }
-        return totalH;
-    }
-    public int minEatingSpeed(int[] v, int h) {
-         int low = 1, high = findMax(v);
 
-        //apply binary search:
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int totalH = calculateTotalHours(v, mid);
-            if (totalH <= h) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        static int calculateTotalH(int[] piles, int hourly){
+            int totalH = 0;
+            int n = piles.length;
+
+            for(int i=0; i<n; i++){
+                totalH += Math.ceil((double)(piles[i])/(double)(hourly));
+            }
+
+            return totalH;
+        }
+        
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = findMax(piles);
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            int totalH = calculateTotalH(piles, mid);
+
+            if(totalH<=h){
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
+
         return low;
     }
 }
