544e781130b41ef53b9f248e8167f7cd9d94b37a
Rishabh Shukla <117344508+rishabh02dev@users.noreply.github.com>
2024-07-12 23:09:34 +0530

    Time: 1 ms (72.11%) | Memory: 41 MB (16.76%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,8 +1,6 @@
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (divisor == 0) {
-            throw new ArithmeticException("Divisor cannot be zero");
-        }
+       
         
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE; // overflow case
