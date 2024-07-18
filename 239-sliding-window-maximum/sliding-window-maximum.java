8fe3f82ff5aa2ad6fa4fe74b0199fee44fc04b5f
Rishabh Shukla <117344508+rishabh02dev@users.noreply.github.com>
2024-07-18 16:43:03 +0530

    Time: 31 ms (63.21%) | Memory: 60.9 MB (28.37%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,25 +1,29 @@
 class Solution {
     public int[] maxSlidingWindow(int[] a, int k) {
         int n = a.length;
-        int[] r = new int[n - k + 1];
-        int ri = 0;
-        // store index
-        Deque < Integer > q = new ArrayDeque < > ();
-        for (int i = 0; i < a.length; i++) {
-            // remove numbers out of range k
-            if (!q.isEmpty() && q.peek() == i - k) {
-                q.poll();
+        if (n == 0) return new int[0];
+        int[] ans = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        
+        for (int i = 0; i < n; i++) {
+            // Remove indices of elements not in the current sliding window
+            if (!deque.isEmpty() && deque.peekFirst() == i - k) {
+                deque.pollFirst();
             }
-            // remove smaller numbers in k range as they are useless
-            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
-                q.pollLast();
+            
+            // Remove indices of all elements smaller than the current element
+            while (!deque.isEmpty() && a[deque.peekLast()] <= a[i]) {
+                deque.pollLast();
             }
-
-            q.offer(i);
+            
+            deque.offerLast(i);
+            
+            // Add the current maximum to the answer array
             if (i >= k - 1) {
-                r[ri++] = a[q.peek()];
+                ans[i - k + 1] = a[deque.peekFirst()];
             }
         }
-        return r;
+        
+        return ans;
     }
 }
