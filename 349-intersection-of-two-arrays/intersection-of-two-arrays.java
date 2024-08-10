fba7d4257bb396479d10e3054bdf5f65306caee8
Rishabh Shukla <117344508+rishabh02dev@users.noreply.github.com>
2024-08-10 11:15:15 +0530

    Time: 3 ms (55.75%) | Memory: 43.2 MB (43.86%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,36 +1,29 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-
-public class Solution {
+class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        ArrayList<Integer> ans = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
+        ArrayList<Integer> al = new ArrayList<>();
+        int i=0 , j = 0;
+         while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
                 i++;
-            } else if (nums2[j] < nums1[i]) {
+            }
+            else if(nums2[j]<nums1[i]){
                 j++;
-            } else {
-                // Check for duplicates before adding to the result
-                if (ans.isEmpty() || ans.get(ans.size() - 1) != nums1[i]) {
-                    ans.add(nums1[i]);
+            }else{
+                if(al.size()==0 || al.get(al.size()-1)!=nums1[i]){
+                    al.add(nums1[i]);
                 }
                 i++;
                 j++;
             }
         }
 
-        // Convert ArrayList to array
-        int[] result = new int[ans.size()];
-        for (int k = 0; k < ans.size(); k++) {
-            result[k] = ans.get(k);
+        int arr[] = new int[al.size()];
+        for(int k=0; k<arr.length;k++){
+            arr[k] = al.get(k);
         }
 
-        return result;
+        return arr;
     }
 }
