af39aee424266663f94d71adcfbbf94ad446f5ee
Rishabh Shukla <117344508+rishabh02dev@users.noreply.github.com>
2024-08-16 00:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (54.24%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,27 +1,49 @@
 class Solution {
     public void sortColors(int[] arr) {
         int n = arr.length;
-        int cnt0 = 0;
-        int cnt1 = 0;
-        int cnt2  = 0;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
         for(int i=0; i<n;i++){
             if(arr[i]==0){
-                cnt0++;
-            }else if(arr[i]==1){
-                cnt1++;
-            }else{
-                cnt2++;
+                zero++;
+            }
+            else if(arr[i]==1){
+                one++;
+            }
+            else{
+                two++;
             }
         }
-        
-        for(int i=0; i<cnt0;i++){
+
+        for(int i=0; i<zero;i++){
             arr[i] = 0;
         }
-        for(int i=cnt0; i<cnt0+cnt1;i++){
+        for(int i=zero;i<zero+one;i++){
             arr[i] = 1;
         }
-        for(int i=cnt0+cnt1; i<n;i++){
+        for(int i=zero+one; i<n;i++){
             arr[i] = 2;
         }
     }
 }
+
+
+//now we are using the dutch flag algorithm
+
+// int low = 0, mid = 0, high = n-1;
+// while(mid<=high){
+//     if(arr[mid]==0){
+//         //swap(arr[mid],arr[low]);
+//         left++;
+//         mid++;
+//     }
+//     else if(arr[mid]==1){
+//         mid++;
+//     }
+//     else{
+//         swap(arr[mid],arr[high]);
+//         high--;
+//     }
+// }
