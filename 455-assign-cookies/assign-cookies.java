9de3049dacd73cc1560da497943fd6314cd1d71c
Rishabh Shukla <117344508+rishabh02dev@users.noreply.github.com>
2024-08-21 19:40:23 +0530

    Time: 9 ms (34.32%) | Memory: 45.5 MB (20.57%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -5,10 +5,10 @@ class Solution {
 
         int i = 0;
         int j = 0;
-        int count = 0;
+        // int count = 0;
         while(i<g.length && j<s.length){
             if(g[i]<=s[j]){
-                count++;
+                // count++;
                 i++;
                 j++;
             }
@@ -16,6 +16,6 @@ class Solution {
                 j++;
             }
         }
-        return count;
+        return i;
     }
 }
